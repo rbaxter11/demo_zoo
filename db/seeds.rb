962dffe0899f289041fb6ebdbb5acae62020e0a0
ryan <ryanandrewbaxter@gmail.com>
2020-10-29 17:04:28 -0500

    Adds dbseed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Animal.create(species: "Giraffe", age: 12, food: "Tall leaves")
+# Animal.create(species: "Unicorn", age: 83, food: "Magic mushrooms")
+# Animal.create(species: "Miniature Horse", age: 1, food: "Milk")
